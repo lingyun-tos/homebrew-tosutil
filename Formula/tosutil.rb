09faf298b66e9729f4a93e70ac07a475e23f6495
lingyun-tos <liulingyun.888@bytedance.com>
2025-10-21 17:53:05 +0800

    Update tosutil.rb

--- Formula/tosutil.rb
+++ Formula/tosutil.rb
@@ -1,14 +1,14 @@
-class YourTool < Formula
+class Tosutil < Formula
   desc "tosutil is a command-line tool for accessing and managing TOS(Torch Object Storage) resource in the volcengine"
   homepage "https://www.volcengine.com/docs/6349/148775?lang=en"
   version "4.1.2"
   
   if Hardware::CPU.arm?
     url "https://github.com/volcengine/tosutil/releases/download/v4.1.2/tosutil-mac-arm64.zip"
-    sha256 "PASTE_ARM64_SHA256_HERE"
+    sha256 "dffdc78932c3c2c23cc1d4015d7a32be72d4cfe9cd1ca1cc69b36a86bc8dc272"
   else
     url "https://github.com/volcengine/tosutil/releases/download/v4.1.2/tosutil-mac-amd64.zip"
-    sha256 "PASTE_AMD64_SHA256_HERE"
+    sha256 "8da4628a3178944d8c56814478d1ee3f935befd91ef248ebf3338e9ffb65c1ca"
   end
 
   def install
